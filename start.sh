ab4f1d8105019f8bd38b0ae9e38ca8ea7e5acec3
Yin <yin@yinn.dev>
2026-03-14 07:53:00 +0200

    chore: remove the start.sh file

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# install .NET first
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-bash dotnet-install.sh --channel 10.0
-export PATH=$HOME/.dotnet:$PATH
-
-# build & run
-dotnet publish TickrServer/TickrServer.csproj -c Release -o ./publish
-dotnet ./publish/TickrServer.dll
